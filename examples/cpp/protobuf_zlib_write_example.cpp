12c3518896b290f4cc23c7bf4278a251b3a1be43
Jaime Soto <jsoto@simverge.com>
2018-11-29 22:23:34 -0500

    Implement write example using a custom blob proto file
    Caller must provide two arguments: an input data file path and an output compressed protobuf file path

--- examples/cpp/protobuf_zlib_write_example.cpp
+++ examples/cpp/protobuf_zlib_write_example.cpp
@@ -22,7 +22,69 @@
  * SOFTWARE.
  */
 
+
+
+#include "examples/cpp/blob.pb.h"
+
+#include <google/protobuf/io/zero_copy_stream_impl.h>
+#include <google/protobuf/io/zero_copy_stream_impl_lite.h>
+#include <google/protobuf/io/gzip_stream.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+
 int main(int argc, char **argv)
 {
+	GOOGLE_PROTOBUF_VERIFY_VERSION;
+
+	if (argc < 3)
+	{
+		std::cerr << "Usage: " << argv[0] << " input-data-file output-blob-file" << std::endl;
+		return -1;
+	}
+
+	char *dataPath = argv[1];
+	char *blobPath = argv[2];
+
+	// Read the data into the blob and write the blob.
+	simverge::Blob blob;
+	std::ifstream in(dataPath, std::ios::binary);
+
+	if (in)
+	{
+		*blob.mutable_source() = dataPath;
+		auto data = blob.mutable_data();
+
+		in.unsetf(std::ios::skipws);
+		in.seekg(0, std::ios::end);
+		data->reserve(in.tellg());
+		in.seekg(0, std::ios::beg);
+		
+		data->assign(std::istreambuf_iterator<char>(in), std::istreambuf_iterator<char>());
+		in.close();
+		std::cout << "Creating new blob with " << data->size() << " bytes read from " << dataPath << std::endl;
+
+		auto uncompressedBytes = blob.ByteSizeLong();
+		std::cout << "Uncompressed blob size (source path and data): " << uncompressedBytes << " bytes" << std::endl;
+
+		std::unique_ptr<char[]> buffer(new char[uncompressedBytes]);
+		google::protobuf::io::ArrayOutputStream aos(buffer.get(), (int) uncompressedBytes);
+		google::protobuf::io::GzipOutputStream gos(&aos);
+
+		if (blob.SerializeToZeroCopyStream(&gos))
+		{
+			gos.Close();
+			auto compressedBytes = aos.ByteCount();
+
+			std::ofstream out(blobPath);
+			out.write(buffer.get(), compressedBytes);
+
+			std::cout << "Wrote compressed blob of size " << compressedBytes << " bytes ("
+				<< (100.0 * (uncompressedBytes - compressedBytes) / uncompressedBytes) << "% compression ratio): "
+				<< blobPath << std::endl;
+		}
+	}
+
 	return 0;
 }
